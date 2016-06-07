21abc536769650f7b0a3937fe3ebd86f53201227
Kristian Cox <kriss@gritdigital.com>
2016-06-07 11:37:27 +0100

    feat: add more forms content

--- demo.php
+++ demo.php
@@ -32,6 +32,47 @@ function lorem($paragraphs = 1, $reorder = true)
     </head>
     <body style="background:#eee">
         <div class="container">
+            <div class="breadcrumbs">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Foo</a></li>
+                    <li><a href="#">Bar</a></li>
+                    <li>Baz</li>
+                </ul>
+            </div>
+            <div class="margin-bottom"></div>
+            <div class="grid grid--gutters">
+                <div class="grid__col grid__col--sm-12 grid__col--lg-8">
+                    <div class="card">
+                        <?=lorem(4, 1)?>
+                    </div>
+                </div>
+                <div class="grid__col grid__col--sm-12 grid__col--lg-4">
+                    <div class="card">
+                        <div class="card__header">
+                            <h3 class="card__title">Contact us</h3>
+                        </div>
+                        <div class="card__body">
+                            <form class="form" action="#">
+                                <div class="grid grid--gutters">
+                                    <div class="grid__col grid__col--sm-6">
+                                        <div class="form__group">
+                                            <label for="">First Name</label>
+                                            <input type="text">
+                                        </div>
+                                    </div>
+                                    <div class="grid__col grid__col--sm-6">
+                                        <div class="form__group">
+                                            <label for="">Last Name</label>
+                                            <input type="text">
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="margin-bottom"></div>
             <div class="grid grid--gutters">
                 <div class="grid__col grid__col--sm-6 grid__col--md-4 grid__col--lg-3 margin-bottom">
@@ -193,6 +234,17 @@ function lorem($paragraphs = 1, $reorder = true)
                     </tr>
                 </tbody>
             </table>
+            <ul class="pagination">
+                <li class="pagination__prev"><a href="#"></a></li>
+                <li><a href="#">1</a></li>
+                <li class="pagination__active">2</li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li class="pagination__dots"></li>
+                <li><a href="#">12</a></li>
+                <li><a href="#">13</a></li>
+                <li class="pagination__next"><a href="#"></a></li>
+            </ul>
         </div>
     </body>
 </html>
